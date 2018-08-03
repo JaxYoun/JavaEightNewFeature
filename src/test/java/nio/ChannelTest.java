3ed828e303dbbd0581ea92860eeea2c5ac806998
JaxYoun <gao88jie@qq.com>
2018-08-04 00:09:39 +0800

    添加了通道的分散读和汇聚写、字符集操作

--- src/test/java/nio/ChannelTest.java
+++ src/test/java/nio/ChannelTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -14,8 +15,8 @@ import java.nio.file.StandardOpenOption;
 /**
  * @Auther: Yang
  * @Date: 2018/8/2 23:13
- * @Description:
- * 一、用于源节点和目标节点的连接，在JavaNIO里负责Buffer中数据的传输，需要配合Buffer才能传输数据。
+ * @Description: 一、用于源节点和目标节点的连接，在JavaNIO里负责Buffer中数据的传输，需要配合Buffer才能传输数据。
+ * <p>
  * 二、|--FileChannel  //用于文件IO
  * |--SocketChannel  //用于TCP客户端
  * |--ServerSocketChannel  //用于TCP服务器端
@@ -33,9 +34,14 @@ import java.nio.file.StandardOpenOption;
  * DatagramSocket
  * 2.JDK1.7后，提供了NIO.2针对各个通道类提供了静态的open()方法
  * 3.JDK1.7后，提供了NIO.2 Files工具类提供了newByteChannel()方法
+ * <p>
  * 四、通道间的数据传输
  * transferFrom()
  * tranferTo()
+ * <p>
+ * 五、分散读取与聚集写入（不支持直接缓冲区）
+ * Scatter：将一个通道中的数据从前到后依此按缓冲区容量分散到多个缓冲区中
+ * Gathering：将多个缓冲区中的数据按先后顺序依此聚集写入到一个通道中
  */
 public class ChannelTest {
 
@@ -51,7 +57,7 @@ public class ChannelTest {
                 FileOutputStream out = new FileOutputStream("D:\\channel\\nginx.conf2");
                 //1.获取通道
                 FileChannel inChannel = in.getChannel();
-                FileChannel outChannel = out.getChannel();
+                FileChannel outChannel = out.getChannel()
         ) {
             //2.分配指定大小的缓冲区
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
@@ -78,7 +84,7 @@ public class ChannelTest {
         try (
                 //1.获取通道
                 FileChannel inChannel = FileChannel.open(Paths.get("D:\\channel\\nginx.conf"), StandardOpenOption.READ);
-                FileChannel outChannel = FileChannel.open(Paths.get("D:\\channel\\nginx.conf3"), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE);
+                FileChannel outChannel = FileChannel.open(Paths.get("D:\\channel\\nginx.conf3"), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE)
         ) {
             //2.内存映射文件(直接缓冲区)
             MappedByteBuffer inMappedByteBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
@@ -101,11 +107,43 @@ public class ChannelTest {
         try (
                 //1.获取通道
                 FileChannel inChannel = FileChannel.open(Paths.get("D:\\channel\\nginx.conf"), StandardOpenOption.READ);
-                FileChannel outChannel = FileChannel.open(Paths.get("D:\\channel\\nginx.conf4"), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE);
+                FileChannel outChannel = FileChannel.open(Paths.get("D:\\channel\\nginx.conf4"), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE)
         ) {
 //            inChannel.transferTo(0, inChannel.size(), outChannel);  //等效
             outChannel.transferFrom(inChannel, 0, inChannel.size());
 
+        }
+    }
+
+    @Test
+    public void test4() throws IOException {
+        try (
+                //源
+                RandomAccessFile randomAccessFile = new RandomAccessFile("D:\\channel\\nginx.conf", "rw");
+                //目标
+                RandomAccessFile randomAccessFile0 = new RandomAccessFile("D:\\channel\\nginx.conf5", "rw")
+        ) {
+            //1.获取通道
+            FileChannel inChannel = randomAccessFile.getChannel();
+
+            //2.分配指定大小的缓冲区
+            ByteBuffer byteBuffer = ByteBuffer.allocate(2);
+            ByteBuffer byteBuffer0 = ByteBuffer.allocate(8);
+
+            //3.分散读取
+            ByteBuffer[] byteBufferArr = {byteBuffer, byteBuffer0};
+            inChannel.read(byteBufferArr);
+            
+            //将缓冲区转换模式
+            for (ByteBuffer it : byteBufferArr) {
+                it.flip();
+            }
+            System.out.println(new String(byteBufferArr[0].array(), 0, byteBufferArr[0].limit()));
+            System.out.println("==========");
+            System.out.println(new String(byteBufferArr[1].array(), 0, byteBufferArr[1].limit()));
+
+            //4.聚集写入
+            randomAccessFile0.getChannel().write(byteBufferArr);
         }
     }
 
---
+++ src/test/java/nio/CharSetTest.java
@@ -0,0 +1,60 @@
+package nio;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+/**
+ * @Auther: Yang
+ * @Date: 2018/8/3 22:34
+ * @Description:字符集
+ */
+public class CharSetTest {
+
+    /**
+     * 查看支持的字符集
+     */
+    @Test
+    public void test() {
+        Charset.availableCharsets().forEach((a, b) -> System.out.println(a + "-->" + b));
+    }
+
+    @Test
+    public void test0() {
+        Charset charset = Charset.forName("UTF-8");
+        //获取编码器
+        CharsetEncoder encoder = charset.newEncoder();
+        //获取解码器
+        CharsetDecoder decoder = charset.newDecoder();
+
+        CharBuffer charBuffer = CharBuffer.allocate(100);
+        charBuffer.put("啊九零六零");
+        charBuffer.flip();
+
+        ByteBuffer byteBuffer = null;
+        try {
+            //编码
+            byteBuffer = encoder.encode(charBuffer);
+            for (int i = 0; i < 5; i++) {
+                System.out.println(byteBuffer.get());
+            }
+        } catch (CharacterCodingException e) {
+            e.printStackTrace();
+        }
+
+        //解码
+        byteBuffer.flip();
+        try {
+            CharBuffer charBuffer1 = decoder.decode(byteBuffer);
+            System.out.println(charBuffer1.toString());
+        } catch (CharacterCodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
